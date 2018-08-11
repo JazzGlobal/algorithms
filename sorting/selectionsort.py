81e162d65e4fc4934918afc2fcc90c322d850ecc
JazzGlobal <waymer147@gmail.com>
2018-08-11 15:56:26 -0400

    Added recusrive selection sort function

--- sorting/selectionsort.py
+++ sorting/selectionsort.py
@@ -11,3 +11,21 @@ def SelectionSort(data):
         unsorted[unsorted.index(largest)],unsorted[unsortedPartitionIndex - 1] = unsorted[unsortedPartitionIndex - 1],unsorted[unsorted.index(largest)]
         unsortedPartitionIndex = unsortedPartitionIndex - 1
     return unsorted
+
+def RecursiveSelection(data):
+    unsorted = data.copy()
+    unsortedPartitionIndex = len(unsorted)
+
+    def sort(index):
+        largest = unsorted[0]
+        for i in range(1,index):
+            if(unsorted[i] > largest):
+                largest = unsorted[i]
+            else:
+                i = i + 1
+        unsorted[unsorted.index(largest)],unsorted[index - 1] = unsorted[index - 1],unsorted[unsorted.index(largest)]
+        if(index <= 1):
+            return
+        sort(index - 1)
+    sort(unsortedPartitionIndex)
+    return unsorted
