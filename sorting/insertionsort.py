acb927b8dd9f84a94be16c90e625079bacd8e5b7
JazzGlobal <waymer147@gmail.com>
2018-08-11 20:44:42 -0400

    updated insertionsort.py with recursive method

--- sorting/insertionsort.py
+++ sorting/insertionsort.py
@@ -10,3 +10,20 @@ def InsertionSort(data):
         unsorted[i] = newElement
         firstUnsortedIndex+=1
     return unsorted
+
+def RecursiveInsertion(data):
+    unsorted = data.copy()
+    firstUnsortedIndex = 1
+    def sort(index):
+        newElement = unsorted[index]
+        i = index
+        while(i > 0 and unsorted[i - 1] > newElement):
+            print(i)
+            unsorted[i] = unsorted[i - 1]
+            i -= 1
+        unsorted[i] = newElement
+        if(index >= len(unsorted) - 1):
+            return
+        sort(index + 1)
+    sort(firstUnsortedIndex)
+    return unsorted
