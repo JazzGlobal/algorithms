2134a2973d7c6525344db6f6957fd4bf385ca7f2
JazzGlobal <waymer147@gmail.com>
2020-10-20 23:24:21 -0400

    Added Linear Search algorithm.

---
+++ algorithms/searching/linearsearch.py
@@ -0,0 +1,14 @@
+def linear_search(value, list):
+    '''
+    Iterates through the entirety of the given list in search of the given value.
+    Returns the index of the item if found, returns -1 otherwise.
+    
+    :param value: Value being searched for.
+    :param list: List that is being traversed.
+    :return: Index of found value, or -1 if not found. 
+
+    '''
+    for x in list:
+        if(value == list[x-1]):
+            return x - 1
+    return -1
