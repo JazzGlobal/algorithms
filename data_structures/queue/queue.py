f5462dea24a44e5b16fdb98cc039e0f4ba315f3f
JazzGlobal <waymer147@gmail.com>
2020-10-21 17:57:55 -0400

    Created Boiler Plate

---
+++ data_structures/queue/queue.py
@@ -0,0 +1,15 @@
+class Queue: 
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def remove(self):
+        pass
+
+    def peek(self):
+        pass
+    
+    def empty(self):
+        pass
