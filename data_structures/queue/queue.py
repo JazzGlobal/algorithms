1c0958d890149aea87260878b58e155b27a816b6
JazzGlobal <waymer147@gmail.com>
2020-10-21 19:20:57 -0400

    Implemented Queue data structure.

--- data_structures/queue/queue.py
+++ data_structures/queue/queue.py
@@ -1,15 +1,34 @@
 class Queue: 
     def __init__(self):
-        pass
+        self.first = None
+        self.last = None
 
-    def add(self, item):
-        pass
+    def add(self, data):
+        node = QueueNode(data)
+        if(self.last != None):
+            self.last.next = node
+        self.last = node
+        if(self.first == None):
+            self.first = self.last
 
     def remove(self):
-        pass
+        if self.first == None: 
+            raise Exception('Queue is empty. Cannot remove.')    
+        data = self.first.data
+        self.first = self.first.next
+        if self.first == None:
+            self.last = None
+        return data
 
     def peek(self):
-        pass
+        if self.empty():
+            raise Exception('Queue is empty. Cannot peek.')
+        return self.first
     
     def empty(self):
-        pass
+        return self.first == None
+
+class QueueNode: 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
