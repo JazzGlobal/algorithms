8e291c28a8d7bd41f2d234ecbab0259d7c9d1f8a
JazzGlobal <waymer147@gmail.com>
2020-10-26 00:36:44 -0400

    Added tests for test_inorder_traverse.pngs tree for both pre and post traversals.

--- data_structures/trees/tests_tree.py
+++ data_structures/trees/tests_tree.py
@@ -44,6 +44,13 @@ class TestNode(unittest.TestCase):
         self.assertEqual([3,1,5,0,4,2,6], result)
     
     # Preorder Tree Traversal tests.
+    def test_preorder_traverse(self):
+        # Arrange
+        test_node = Node(0, Node(1, Node(3, None, None),None), Node(2, None, Node(4, None, None)))
+        # Act
+        result = tree.preorder_traverse(test_node)
+        # Assert
+        self.assertEqual([0,1,3,2,4], result)
     def test_preorder_traverse_multibranch(self):
         # Arrange
         test_node = Node(1, Node(2, Node(4, None, None), Node(5, None, None)), Node(3, None, None))
@@ -53,6 +60,13 @@ class TestNode(unittest.TestCase):
         self.assertEqual([1,2,4,5,3], result)
 
     # Postorder Tree Traversal tests.
+    def test_postorder_traverse(self):
+        # Arrange
+        test_node = Node(0, Node(1, Node(3, None, None),None), Node(2, None, Node(4, None, None)))
+        # Act
+        result = tree.postorder_traverse(test_node)
+        # Assert
+        self.assertEqual([3,1,4,2,0], result)
     def test_postorder_traverse_multibranch(self):
         # Arrange
         test_node = Node(1, Node(2, Node(4, None, None), Node(5, None, None)), Node(3, None, None))
